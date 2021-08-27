de307182e02db53e187bf7ff3319b6127bf19c1d
dlamarca1 <danielrlamarca@hotmail.com>
2021-08-26 21:00:56 -0300

    Bettered some codding

--- src/main/java/com/dio/project3/model/Product.java
+++ src/main/java/com/dio/project3/model/Product.java
@@ -1,6 +1,9 @@
 package com.dio.project3.model;
 
+import lombok.AllArgsConstructor;
+import lombok.Builder;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 import org.springframework.data.elasticsearch.annotations.Document;
 
 import javax.persistence.Entity;
